29ecaa2e68af18c42416a2e6169416adee484ba4
m-abdelwahab <hey@mahmoud.codes>
2022-01-20 18:27:50 +0200

    add selection styles

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 }
 export const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className=" dark:text-gray-100 text-gray-900 bg-gray-50 dark:bg-gray-1000  px-4">
+    <div className="dark:selection:bg-gray-100 dark:selection:text-gray-900 selection:bg-gray-800 selection:text-gray-100 dark:text-gray-100 text-gray-900 bg-gray-50 dark:bg-gray-1000  px-4">
       <Nav />
       <main className="max-w-screen-lg mx-auto">{children}</main>
       <Footer />
